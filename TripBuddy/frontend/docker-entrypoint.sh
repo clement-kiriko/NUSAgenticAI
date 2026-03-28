7cffbd5043bfe1198f4d66266a701a3feb72e51e
clement-kiriko <clement_tyh@icloud.com>
2026-03-29 01:45:03 +0800

    fix frontend crashing in docker swarm

--- TripBuddy/frontend/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-# Substitute only ${API_UPSTREAM} — all nginx variables ($host, $uri, etc.) are left intact.
-envsubst '${API_UPSTREAM}' \
-    < /etc/nginx/conf.d/default.conf.template \
-    > /etc/nginx/conf.d/default.conf
-exec nginx -g 'daemon off;'
